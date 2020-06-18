c7054f629422f4e49de48126d56fc78d060ed3be
Wilson <clyeung0714@gmail.com>
2020-06-18 23:01:32 +0800

    amend the UI of ChartActivity and prepare for pie chart

--- app/src/main/java/com/example/personalaccounting/ChartActivity.java
+++ app/src/main/java/com/example/personalaccounting/ChartActivity.java
@@ -8,7 +8,6 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.CompoundButton;
-import android.widget.HorizontalScrollView;
 import android.widget.ImageButton;
 import android.widget.Spinner;
 import android.widget.Switch;
@@ -21,7 +20,9 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
+import com.github.mikephil.charting.animation.Easing;
 import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.components.AxisBase;
 import com.github.mikephil.charting.components.Description;
 import com.github.mikephil.charting.components.Legend;
@@ -38,6 +39,7 @@ import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collections;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 
@@ -48,23 +50,24 @@ public class ChartActivity extends AppCompatActivity {
     public static final float PARAMETER_TEXT_SIZE = 15f;
 
     DatabaseHelper myDB;
-    HorizontalScrollView chartIconView;
     Intent intent;
 
     private BarChart barChart;
-    private YAxis leftAxis;
-    private YAxis rightAxis;
     private XAxis xAxis;
-    private Legend legend;
+    private PieChart pieChart;
 
-    TableLayout chartParameterTable;
     ArrayList<Integer> yearList = new ArrayList<>();
     int selectedYear;
+    int selectedMonth = -1;
     boolean selectedIncome = false;
+    boolean selectedBarChart = true;
     String selectedType;
-    Spinner typeSpinner;
     Switch incomeSwitch;
-
+    Spinner yearSpinner;
+    Spinner monthSpinner;
+    Spinner typeSpinner;
+    ImageButton barChartButton;
+    ImageButton pieChartButton;
     ImageButton chartButton;
     ImageButton homeButton;
     ImageButton settingButton;
@@ -77,13 +80,17 @@ public class ChartActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chart);
 
-
         selectedType = getResources().getString(R.string.all);
 
         intent = getIntent();
-        chartIconView = findViewById(R.id.chart_iconView);
         barChart = findViewById(R.id.barChart_view);
-        chartParameterTable = findViewById(R.id.chart_parameter_table);
+        pieChart = findViewById(R.id.pieChart_view);
+        yearSpinner = findViewById(R.id.chart_yearSpinner);
+        monthSpinner = findViewById(R.id.chart_monthSpinner);
+        typeSpinner = findViewById(R.id.chart_typeSpinner);
+        incomeSwitch = findViewById(R.id.chart_incomeSwitch);
+        barChartButton = findViewById(R.id.barChart_icon);
+        pieChartButton = findViewById(R.id.pieChart_icon);
         chartButton = findViewById(R.id.chart_button);
         homeButton = findViewById(R.id.home_button);
         settingButton = findViewById(R.id.setting_button);
@@ -92,6 +99,9 @@ public class ChartActivity extends AppCompatActivity {
         yearList.add(myDB.getUserStartDate().get(Calendar.YEAR));
         selectedYear = yearList.get(0);
 
+
+        barChartButton.setOnClickListener(new barChartIconOnClickListener());
+        pieChartButton.setOnClickListener(new pieChartIconOnClickListener());
         chartButton.setOnClickListener(new chartButtonOnClickListener());
         homeButton.setOnClickListener(new homeButtonOnClickListener());
         settingButton.setOnClickListener(new settingButtonOnClickListener());
@@ -103,28 +113,8 @@ public class ChartActivity extends AppCompatActivity {
 
         initBarChart();
         showBarChart();
-        initBarChartParameterTable();
-
-    }
-
-    private void initBarChartParameterTable(){
-        chartParameterTable.removeView(chartParameterTable.getRootView());
-        TableRow.LayoutParams rowParams = new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, (int) getResources().getDimension(R.dimen.general_column_height), PARAMETER_ROW_ELEMENT_WEIGHT);
-
-        TableRow yearRow = new TableRow(getApplicationContext());
-        yearRow.setWeightSum(PARAMETER_ROW_WEIGHT_SUM);
-
-        TextView yearText = new TextView(getApplicationContext());
-        yearText.setText(R.string.year);
-        yearText.setTextSize(PARAMETER_TEXT_SIZE);
-        yearText.setGravity(Gravity.CENTER);
-        yearText.setLayoutParams(rowParams);
-
-        Spinner yearSpinner = new Spinner(getApplicationContext());
-        yearSpinner.setLayoutParams(rowParams);
-        yearSpinner.setGravity(Gravity.CENTER);
-
         Calendar lastActiveDate = myDB.getUserLastActiveDate();
+
         int year = yearList.get(0);
         while (year <= lastActiveDate.get(Calendar.YEAR)){
             if(!yearList.contains(year)){
@@ -132,36 +122,41 @@ public class ChartActivity extends AppCompatActivity {
             }
             year++;
         }
-
         Collections.sort(yearList);
         yearSpinner.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, yearList.toArray()));
         yearSpinner.setOnItemSelectedListener(new yearSpinnerOnItemSelectedListener());
 
-        yearRow.addView(yearText);
-        yearRow.addView(yearSpinner);
-        chartParameterTable.addView(yearRow);
+        ArrayList<String> monthList = new ArrayList<>();
+        monthList.add(getResources().getString(R.string.not_available));
+        for(String string: getResources().getStringArray(R.array.monthArray)){
+            monthList.add(string);
+        }
+        monthSpinner.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, monthList.toArray()));
+        monthSpinner.setOnItemSelectedListener(new monthSpinnerOnItemSelectedListener());
 
-        TableRow typeRow = new TableRow(getApplicationContext());
-        typeRow.setWeightSum(PARAMETER_ROW_WEIGHT_SUM);
+        incomeSwitch.setOnCheckedChangeListener(new incomeSwitchOnCheckedChangeListener());
 
-        TextView typeText = new TextView(getApplicationContext());
-        typeText.setText(getResources().getString(R.string.type));
-        typeText.setTextSize(PARAMETER_TEXT_SIZE);
-        typeText.setGravity(Gravity.CENTER);
-        typeText.setLayoutParams(rowParams);
-
-        typeSpinner = new Spinner(getApplicationContext());
         typeSpinner.setAdapter(new ArrayAdapter<>(ChartActivity.this, android.R.layout.simple_spinner_item, expenseTypeList));
-        typeSpinner.setGravity(Gravity.CENTER);
-        typeSpinner.setLayoutParams(rowParams);
         typeSpinner.setOnItemSelectedListener(new typeSpinnerOnItemSelectedListener());
+    }
 
-        typeRow.addView(typeText);
-        typeRow.addView(typeSpinner);
-        chartParameterTable.addView(typeRow);
+    private void initPieChart(){
+        barChart.setVisibility(View.INVISIBLE);
+        pieChart.setVisibility(View.VISIBLE);
+        pieChart.setUsePercentValues(true);
+        pieChart.getDescription().setEnabled(false);
+        pieChart.setDragDecelerationFrictionCoef(0.9f);
+        pieChart.setRotationAngle(0);
+        pieChart.setRotationEnabled(true);
+        pieChart.setHighlightPerTapEnabled(true);
+        pieChart.animateY(1400, Easing.EasingOption.EaseInOutQuad);
+
+
     }
 
     private void initBarChart(){
+        pieChart.setVisibility(View.INVISIBLE);
+        barChart.setVisibility(View.VISIBLE);
         barChart.setDrawGridBackground(false);
         barChart.setDrawBarShadow(false);
         barChart.setHighlightFullBarEnabled(false);
@@ -180,6 +175,10 @@ public class ChartActivity extends AppCompatActivity {
         xAxis.setGranularity(1f);
         xAxis.setDrawAxisLine(false);
         xAxis.setDrawGridLines(false);
+
+        YAxis leftAxis;
+        YAxis rightAxis;
+        Legend legend;
 
         leftAxis = barChart.getAxisLeft();
         leftAxis.setDrawAxisLine(false);
@@ -207,36 +206,66 @@ public class ChartActivity extends AppCompatActivity {
     }
 
     private void showBarChart(){
+        final String[] monthArray = getResources().getStringArray(R.array.monthArray);
         List<Double> dateValueList = getBillDataOfYear(selectedYear, selectedIncome, selectedType);
-
-        xAxis.setValueFormatter(new IAxisValueFormatter() {
-            final String[] monthArray = getResources().getStringArray(R.array.monthArray);
-            @Override
-            public String getFormattedValue(float value, AxisBase axis) {
-                return  monthArray[(int) value];
-            }
-        });
         ArrayList<BarEntry> entries = new ArrayList<>();
-        for (int i = 0; i < dateValueList.size(); i++) {
+        StringBuilder title = new StringBuilder(selectedYear + " ");
 
-            BarEntry barEntry = new BarEntry(i, dateValueList.get(i).floatValue());
-            entries.add(barEntry);
+        if(selectedMonth == 1){
+            GregorianCalendar calendar = new GregorianCalendar();
+            title.append(monthArray[selectedMonth]);
+            if(calendar.isLeapYear(selectedYear)){
+
+            }
+            else{
+
+            }
         }
-
-        String title = "";
-        if(selectedType.equals(getResources().getString(R.string.all))){
-            title = selectedYear + " " + (selectedIncome? getResources().getString(R.string.income):getResources().getString(R.string.expense));
+        else if (selectedMonth == 0 || selectedMonth == 2 || selectedMonth == 4 || selectedMonth == 6 ||
+                selectedMonth == 7 || selectedMonth == 9 || selectedMonth == 11 ){
+            title.append(monthArray[selectedMonth] + " ");
+        }
+        else if(selectedMonth == 3 || selectedMonth == 5 || selectedMonth == 8 || selectedMonth == 10){
+            title.append(monthArray[selectedMonth] + " ");
         }
         else{
-            title = selectedYear + " " + selectedType;
+            xAxis.setValueFormatter(new IAxisValueFormatter() {
+
+                @Override
+                public String getFormattedValue(float value, AxisBase axis) {
+                    return  monthArray[(int) value];
+                }
+            });
+            for (int i = 0; i < dateValueList.size(); i++) {
+
+                BarEntry barEntry = new BarEntry(i, dateValueList.get(i).floatValue());
+                entries.add(barEntry);
+            }
+
         }
 
-        BarDataSet barDataSet = new BarDataSet(entries, title);
+        if(selectedType.equals(getResources().getString(R.string.all))){
+            title.append(selectedIncome? getResources().getString(R.string.income):getResources().getString(R.string.expense));
+        }
+        else{
+            title.append(selectedType);
+        }
+
+        BarDataSet barDataSet = new BarDataSet(entries, title.toString());
         initBarDataSet(barDataSet, ContextCompat.getColor(this, (selectedIncome?R.color.income:R.color.expense)));
 
         BarData data = new BarData(barDataSet);
         data.setBarWidth(0.5f);
         barChart.setData(data);
+    }
+
+    private ArrayList<Double> getBillDataOfMonth(int year, int month, int numberOfDays, boolean income, String selectedType){
+        ArrayList<Double> result = new ArrayList<>();
+        Cursor data = myDB.getBillTable();
+        for(int i = 0; i < numberOfDays; i++){
+            result.add(0.0);
+        }
+        return result;
     }
 
     private ArrayList<Double> getBillDataOfYear(int year, boolean income, String type){
@@ -265,6 +294,36 @@ public class ChartActivity extends AppCompatActivity {
         return result;
     }
 
+    private class barChartIconOnClickListener implements View.OnClickListener{
+
+        @Override
+        public void onClick(View view) {
+            recreate();
+        }
+    }
+
+    private class pieChartIconOnClickListener implements View.OnClickListener{
+
+        @Override
+        public void onClick(View view) {
+
+        }
+    }
+
+    private class incomeSwitchOnCheckedChangeListener implements CompoundButton.OnCheckedChangeListener{
+
+        @Override
+        public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+            if(isChecked) {
+                typeSpinner.setAdapter(new ArrayAdapter<>(ChartActivity.this, android.R.layout.simple_spinner_item, incomeTypeList));
+                selectedIncome = true;
+            }
+            else{
+                typeSpinner.setAdapter(new ArrayAdapter<>(ChartActivity.this, android.R.layout.simple_spinner_item, expenseTypeList));
+                selectedIncome = false;
+            }
+        }
+    }
     private class barChartOnChartValueSelectedListener implements OnChartValueSelectedListener{
 
         @Override
@@ -297,31 +356,37 @@ public class ChartActivity extends AppCompatActivity {
 
         }
     }
-    private class incomeSwitchOnCheckedChangeListener implements CompoundButton.OnCheckedChangeListener{
+
+    private class monthSpinnerOnItemSelectedListener implements AdapterView.OnItemSelectedListener{
 
         @Override
-        public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
-            if(isChecked){
-                selectedIncome = true;
-                typeSpinner.setAdapter(new ArrayAdapter<>(ChartActivity.this, android.R.layout.simple_spinner_item, incomeTypeList));
+        public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
+            selectedMonth = position - 1;
 
-            }
-            else {
-                selectedIncome = false;
-                typeSpinner.setAdapter(new ArrayAdapter<>(ChartActivity.this, android.R.layout.simple_spinner_item, expenseTypeList));
+            if(selectedBarChart){
+                initBarChart();
+                showBarChart();
             }
 
-            initBarChart();
-            showBarChart();
+
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> adapterView) {
+            selectedMonth = -1;
         }
     }
+
     private class yearSpinnerOnItemSelectedListener implements Spinner.OnItemSelectedListener{
 
         @Override
         public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
             selectedYear = Integer.valueOf(adapterView.getItemAtPosition(position).toString());
-            initBarChart();
-            showBarChart();
+            if(selectedBarChart){
+                initBarChart();
+                showBarChart();
+            }
+
         }
 
         @Override
