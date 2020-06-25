b68ffc9232eae65bf1fe46a2104923216a9f7358
Wilson <clyeung0714@gmail.com>
2020-06-25 16:20:07 +0800

    change setting icons

--- app/src/main/java/com/example/personalaccounting/SettingActivity.java
+++ app/src/main/java/com/example/personalaccounting/SettingActivity.java
@@ -10,7 +10,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class SettingActivity extends AppCompatActivity {
 
-    private ImageButton accountSettingButton;
+    private ImageButton budgetSettingButton;
     private ImageButton userSettingButton;
     private ImageButton feedbackButton;
 
@@ -19,11 +19,11 @@ public class SettingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
 
-        accountSettingButton = findViewById(R.id.accountSetting_button);
+        budgetSettingButton = findViewById(R.id.budgetSetting_button);
         userSettingButton = findViewById(R.id.userSetting_button);
         feedbackButton = findViewById(R.id.feedback_button);
 
-        accountSettingButton.setOnClickListener(new accountSettingButtonOnClickListener());
+        budgetSettingButton.setOnClickListener(new budgetSettingButtonOnClickListener());
         userSettingButton.setOnClickListener(new userSettingButtonOnClickListener());
         feedbackButton.setOnClickListener(new feedbackButtonOnClickListener());
     }
@@ -37,7 +37,7 @@ public class SettingActivity extends AppCompatActivity {
         }
     }
 
-    private class accountSettingButtonOnClickListener implements View.OnClickListener {
+    private class budgetSettingButtonOnClickListener implements View.OnClickListener {
         @Override
         public void onClick(View view) {
             Intent intent = new Intent(SettingActivity.this, BudgetSettingActivity.class);
